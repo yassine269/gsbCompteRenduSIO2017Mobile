39c92576076ec5c4d126e73de308dbb23f7a1330
yassine269 <yassineol@hotmail.fr>
2017-03-23 18:00:02 +0100

    ListView OK , Prise en compte du click sur la ListView en cours.

--- app/src/main/java/bts/sio/compterendu/ConsulterCRActivity.java
+++ app/src/main/java/bts/sio/compterendu/ConsulterCRActivity.java
@@ -1,7 +1,11 @@
 package bts.sio.compterendu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -9,6 +13,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,48 +27,56 @@ import static bts.sio.compterendu.R.id.numRap;
 public class ConsulterCRActivity extends AppCompatActivity {
 
     ListView mListView;
-    String[] prenoms = new String[]{
-            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
-            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
-            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
-            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
-            "Yann", "Zoé"
-    };
+    List listCompteRendu = new ArrayList();
+    int numRapSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultercr);
 
-
-        mListView = (ListView) findViewById(R.id.listView);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ConsulterCRActivity.this,
-                android.R.layout.simple_list_item_1, prenoms);
-        mListView.setAdapter(adapter);
-
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         AdressBookApi service = retrofit.create(AdressBookApi.class);
-        String textTest = ((EditText) findViewById(numRap)).getText().toString();
-        service.getUnCompteRendu(Integer.parseInt(textTest)).enqueue(new Callback<CompteRendu>() {
+        service.getCompteRenduList().enqueue(new Callback<List<CompteRendu>>() {
             @Override
-            public void onResponse(Call<CompteRendu> call, Response<CompteRendu> response) {
-                ((TextView) findViewById(R.id.numRap)).setText(response.body().getNumRap());
-                ((TextView) findViewById(R.id.praticien)).setText(response.body().getPraticien());
-                ((TextView) findViewById(R.id.dateRap)).setText(response.body().getDateRap().toString());
-                //((Spinner) findViewById(R.id.motifSpinner)).get(response.body().getMotifVisit());
-                ((TextView) findViewById(R.id.bilan)).setText(response.body().getBilan());
-                //((TextView) findViewById(R.id.echantSpinner)).setText(response.body().getNomEchant());
-                ((TextView) findViewById(R.id.quant)).setText(response.body().getNbEchant());
+            public void onResponse(Call<List<CompteRendu>> call, Response<List<CompteRendu>> response) {
+                for (CompteRendu compteRendu:response.body()) {
+                    listCompteRendu.add(compteRendu.getNumRap());
+                }
             }
 
             @Override
-            public void onFailure(Call<CompteRendu> call, Throwable t) {
+            public void onFailure(Call<List<CompteRendu>> call, Throwable t) {
+                Log.i("retrofit","Erreur lors de l'ajout d'un compte rendu à la ListView");
+            }
+        });
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ConsulterCRActivity.this,
+                android.R.layout.simple_list_item_1, listCompteRendu);
+        mListView.setAdapter(adapter);
+
+
+        final ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setClickable(true);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+                Object o = lv.getItemAtPosition(position);
+
+                Intent page_suivante = new Intent(getApplicationContext(),SaisieCRActivity.class);
+                page_suivante.putExtra("numRapSelected", o.numRap.toString());
+                startActivity(page_suivante);
 
             }
         });
+
+
     }
 }
--- app/src/main/java/bts/sio/compterendu/SaisieCRActivity.java
+++ app/src/main/java/bts/sio/compterendu/SaisieCRActivity.java
@@ -3,7 +3,17 @@ package bts.sio.compterendu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static bts.sio.compterendu.R.id.numRap;
 
 public class SaisieCRActivity extends AppCompatActivity {
 
@@ -11,5 +21,30 @@ public class SaisieCRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saisiecr);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        AdressBookApi service = retrofit.create(AdressBookApi.class);
+        String textTest = ((EditText) findViewById(numRap)).getText().toString();
+        service.getUnCompteRendu(Integer.parseInt(textTest)).enqueue(new Callback<CompteRendu>() {
+            @Override
+            public void onResponse(Call<CompteRendu> call, Response<CompteRendu> response) {
+                ((TextView) findViewById(R.id.numRap)).setText(response.body().getNumRap());
+                ((TextView) findViewById(R.id.praticien)).setText(response.body().getPraticien());
+                ((TextView) findViewById(R.id.dateRap)).setText(response.body().getDateRap().toString());
+                //((Spinner) findViewById(R.id.motifSpinner)).get(response.body().getMotifVisit());
+                ((TextView) findViewById(R.id.bilan)).setText(response.body().getBilan());
+                //((TextView) findViewById(R.id.echantSpinner)).setText(response.body().getNomEchant());
+                ((TextView) findViewById(R.id.quant)).setText(response.body().getNbEchant());
+            }
+
+            @Override
+            public void onFailure(Call<CompteRendu> call, Throwable t) {
+                Log.i("retrofit","Erreur lors de l'ajout d'un compte rendu à la page de saisie");
+            }
+        });
+
     }
 }
